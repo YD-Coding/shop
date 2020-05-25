dc38a3a5b9cc453308473dfcbb3a567ae02c5a02
Dan0606 <65502527+Dan0606@users.noreply.github.com>
2020-05-25 18:40:38 +0300

    buttons

---
+++ classes_button.py
@@ -0,0 +1,52 @@
+import pygame
+#from harry_flying import score
+import random
+pygame.init()
+
+class Button():
+    def __init__(self, color, x, y, width, height, text, size):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.size = size
+
+    def draw(self,win,outline=None):
+        #Call this method to draw the button on the screen
+        if outline:
+            pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
+            
+        pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
+        
+        if self.text != '':
+            font = pygame.font.SysFont('arial', self.size)
+            text = font.render(self.text, 1, (0,0,0))
+            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def isOver(self, pos):
+        #Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+        return False
+
+# class Power():
+#     def __init__(self, image, timer, randomTimeRange, randomPlace, distance=''):
+#         self.image = image
+#         self.timer = timer
+#         self.randomTimeRange = random.randint(200, 250)
+#         self.randomPlace = randomPlace
+#         self.distance = None
+#     def drop(self):
+#         if self.timer != self.randomTimeRange and score > 15:
+#             self.timer += 1
+#         elif self.timer == self.randomTimeRange:
+#             return
+#         if self.distance < 50:
+#             HasBoost = True
+
+
+    
+    
